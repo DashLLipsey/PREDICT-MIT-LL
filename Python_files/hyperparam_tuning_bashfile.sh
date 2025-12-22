78bb8869def2ffcad1f364a85ff7948704082c7b
Dashiell Lipsey <dlipsey@login-vscode.turing.wpi.edu>
2025-12-22 11:58:31 -0500

    SBATCH file

---
+++ Python_files/hyperparam_tuning_bashfile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=Conditional_encoder_hyperparam_tuning
+#SBATCH --output=/home/dlipsey/MITLincolnLabs/logs/cond_encoder1234e1e2_hyperparam_tuning_%j.out
+#SBATCH --time=00:00:10
+#SBATCH --partition=short
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=8
+
+
+# Create logs directory if it doesn't exist
+mkdir -p /home/dlipsey/MITLincolnLabs/logs
+
+# Print some info
+echo "Job ID: $SLURM_JOB_ID"
+echo "Running on: $(hostname)"
+echo "Starting at: $(date)"
+echo "GPU: $CUDA_VISIBLE_DEVICES"
+
+# Run the bijection-based IMS spectra experiment
+source /home/dlipsey/MITLincolnLabs/.venv/bin/activate #fix directory
+cd /home/dlipsey/MITLincolnLabs/Python_files
+python file.py
+
+echo "Finished at: $(date)"
